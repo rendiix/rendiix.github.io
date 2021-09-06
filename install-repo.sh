f94a6a34a297185caf813e61867a99c84c181bfe
rendiix <vanzdobz@gmail.com>
2021-09-06 13:32:38 +0700

    update ppa install script

--- install-repo.sh
+++ install-repo.sh
@@ -3,16 +3,14 @@
 # Author     : rendiix <vanzdobz@gmail.com>
 # Create date:  5-Jul-2019 16:11
 # Get some needed tools. coreutils for mkdir command, gnugp for the signing key, and apt-transport-https to actually connect to the repo
-apt-get update
-apt-get  --assume-yes upgrade 
-apt-get  --assume-yes install coreutils gnupg wget 
+pkg i gnupg wget -y
 # Make the sources.list.d directory
 [ ! -d $PREFIX/etc/apt/sources.list.d ] && mkdir $PREFIX/etc/apt/sources.list.d
 # Write the needed source file
 if [ ! -f "$PREFIX/etc/apt/sources.list.d/rendiix.list" ]; then
-echo -e "deb https://rendiix.github.io android-tools termux" > $PREFIX/etc/apt/sources.list.d/rendiix.list
-wget -qP $PREFIX/etc/apt/trusted.gpg.d https://rendiix.github.io/rendiix.gpg
-apt update
+	echo -e "deb https://rendiix.github.io android-tools termux" > $PREFIX/etc/apt/sources.list.d/rendiix.list
+	wget -qP $PREFIX/etc/apt/trusted.gpg.d https://rendiix.github.io/rendiix.gpg
+	apt update
 else
-echo "repo already installed"
+	echo "repo already installed"
 fi
